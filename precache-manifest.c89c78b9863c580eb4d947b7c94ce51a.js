ac13cb33d684f8a24c5a6d9371c8d20ee2614de4
huongnguyen <huonghuonglacey1020@gmail.com>
2020-05-14 12:39:10 -0700

    Updates

---
+++ precache-manifest.c89c78b9863c580eb4d947b7c94ce51a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72b9639819a8fb3bde0c77262bc15d68",
+    "url": "/deploy/index.html"
+  },
+  {
+    "revision": "a8abaa135f7b04ff4937",
+    "url": "/deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "76fe8f9686565956a899",
+    "url": "/deploy/static/js/2.03ce0df4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy/static/js/2.03ce0df4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8abaa135f7b04ff4937",
+    "url": "/deploy/static/js/main.ff99802e.chunk.js"
+  },
+  {
+    "revision": "ebd6c2b9aeac916977d6",
+    "url": "/deploy/static/js/runtime-main.99e8b667.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
